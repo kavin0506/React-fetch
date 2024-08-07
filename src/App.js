4e54cd65b0d054c86030d64e573558a166eee399
kavin0506 <kavinmoorthy@gmail.com>
2024-08-07 16:36:40 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Axios from './Axios';
+import Button from './Button';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+import From from './Form';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Login/>
+      <Register/> */}
+      {/* <Button/> */}
+      {/* <From/> */}
+      <Axios/>
+
+    </div>
+  );
+}
+
+export default App;
